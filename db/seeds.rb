eb40b962544a7560c3fc4c283967a7287791c515
Jaradus <ethanschmertzler@mac.com>
2014-02-26 16:17:18 -0500

    Added faker and foundation gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "==========================="
+puts "Seed Task Started"
+puts "==========================="
+
+User.delete_all
+puts "[Notice] Users deleted"
+
+20.times do
+  User.create({
+    
+    })
+end
+
+
+
+puts "==========================="
+puts "Seed Task Complete"
+puts "==========================="
