553ba38e5c9277463ac1ea9068b193553c3413ab
dbushy727 <d.bushkanets@gmail.com>
2014-02-28 16:34:05 -0500

    fixing sticky footer for mobile view

--- app/assets/javascripts/mobile_app.js
+++ app/assets/javascripts/mobile_app.js
@@ -17,15 +17,47 @@ var Splash = Backbone.View.extend({
 
 var SelectWorkout = Backbone.View.extend({
   initialize: function(){
-    this.addEventListeners()
+    this.render();
   },
-
-  addEventListeners: function(){
-    
+  render: function(){
+    var self = this;
+    $.ajax({
+      url:      '/workouts',
+      method:   'GET',
+      dataType: 'json',
+      success: function(data){
+        var source       = $('#workout_template_mobile').html();
+        var template     = Handlebars.compile(source);
+        var templateData = template(data);
+        $('#workout_content_mobile').append(templateData);
+      }
+    })  
   }
 })
+var SelectExercise = Backbone.View.extend({
+  initialize: function(){
+    this.render();
+  },
+  render: function(){
+    var self = this;
+    $.ajax({
+      url:      '/exercises',
+      method:   'GET',
+      dataType: 'json',
+      success: function(data){
+        var source       = $('#exercise_template_mobile').html();
+        var template     = Handlebars.compile(source);
+        var templateData = template(data);
+        $('#exercise_content_mobile').append(templateData);
+      }
+    }) 
+  }
 
+})
 
 $(function(){
+  window.select_workout = new SelectWorkout();
+  window.select_exercise = new SelectExercise();
   window.splash_page = new Splash();
+
 })
