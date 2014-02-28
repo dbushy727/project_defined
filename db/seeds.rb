10ece73cacfc92515f434e327739cf608b073fff
dbushy727 <d.bushkanets@gmail.com>
2014-02-28 17:21:46 -0500

    working on exercise view for mobile

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,6 @@ puts "[Notice] Users deleted"
   last_name   = Faker::Name.last_name
   password    = "password123"
   email       = Faker::Internet.email(first_name+" "+last_name)
-
   new_user = User.create({
               email:      email,
               password:   password,
@@ -19,9 +18,9 @@ puts "[Notice] Users deleted"
               last_name:  last_name
               })
   puts "[User] #{new_user.username} created"
-
+# ===========================================
+# ===========================================
   exercise_list = ['Standard Push-Ups', 'Wide Front Pull-Ups', 'Wide Fly Push-Ups', 'Diamond Push-Ups', 'Back Flys', 'Chair Dips', 'Deep Swimmer Presses', 'Static Arm Curls', 'Seated Two-Angle Shoulder Flys']
-
   exercise_list.each do |exercise|
     new_exercise = Exercise.create({
                                     title:    exercise,
@@ -29,9 +28,9 @@ puts "[Notice] Users deleted"
                                     })
     puts "[Exercise] #{new_exercise.title} added to #{new_exercise.user.first_name}"
   end
-
+# ===========================================
+# ===========================================
   workout_list = ['Cardio', 'Lifting', 'Yoga']
-
   workout_list.each do |workout|
     new_workout = Workout.create({
                                   title: workout,
