80fe964854fc44559c8c1234806c4cee3335682f
Jaradus <ethanschmertzler@mac.com>
2014-03-02 17:14:05 -0500

    Calendar of recent workouts added

--- app/assets/javascripts/history.js
+++ app/assets/javascripts/history.js
@@ -1,58 +1,92 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+// Calendar heatmap of workout history
 
-var parseDate = d3.time.format("%d-%b-%y").parse;
+var cal = new CalHeatMap();
 
-var x = d3.time.scale()
-    .range([0, width]);
+// Set start date for calendar to five months prior to the present date
+  var d = new Date();
+  d.setMonth(d.getMonth()-5);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+cal.init({
+  domain: "month",
+  data: '/workouts/history',
+  subDomain: "x_day",
+  itemName: ["set", "sets"],
+  subDomainTitleFormat: {
+                        empty: "No workouts recorded on this date"
+                        },
+                        start: d,
+  highlight: ["now"],
+  legendVerticalPosition: "center",
+  legendOrientation: "vertical",
+  legendMargin: [0, 10, 0, 0],
+  cellSize: 15, subDomainTextFormat: "%d",
+  range: 6,
+  tooltip: true,
+  displayLegend: true,
+  domainMargin: 10
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+});
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
 
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
 
-var svg = d3.select("#d3_visualization").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// d3.tsv("data.tsv", function(error, data) {
-//   data.forEach(function(d) {
-//     d.date = parseDate(d.date);
-//     d.close = +d.close;
-//   });
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+// var margin = {top: 20, right: 20, bottom: 30, left: 50},
+//     width = 960 - margin.left - margin.right,
+//     height = 500 - margin.top - margin.bottom;
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Weight (lb)");
+// var parseDate = d3.time.format("%d-%b-%y").parse;
 
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
+// var x = d3.time.scale()
+//     .range([0, width]);
+
+// var y = d3.scale.linear()
+//     .range([height, 0]);
+
+// var xAxis = d3.svg.axis()
+//     .scale(x)
+//     .orient("bottom");
+
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left");
+
+// var line = d3.svg.line()
+//     .x(function(d) { return x(d.date); })
+//     .y(function(d) { return y(d.close); });
+
+// var svg = d3.select("#d3_visualization").append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// // d3.tsv("data.tsv", function(error, data) {
+// //   data.forEach(function(d) {
+// //     d.date = parseDate(d.date);
+// //     d.close = +d.close;
+// //   });
+
+//   x.domain(d3.extent(data, function(d) { return d.date; }));
+//   y.domain(d3.extent(data, function(d) { return d.close; }));
+
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis);
+
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis)
+//     .append("text")
+//       .attr("transform", "rotate(-90)")
+//       .attr("y", 6)
+//       .attr("dy", ".71em")
+//       .style("text-anchor", "end")
+//       .text("Weight (lb)");
+
+//   svg.append("path")
+//       .datum(data)
+//       .attr("class", "line")
+//       .attr("d", line);
--- app/controllers/workout_sessions_controller.rb
+++ app/controllers/workout_sessions_controller.rb
@@ -1,16 +1,36 @@
 class WorkoutSessionsController < ApplicationController
 
   def index
+    users_workouts = WorkoutSession.where(user_id: current_user.id)
+    users_workouts = WorkoutSession.where(user_id: 1)
 
+    exercise_instances_per_each_workout_session = {}
+
+
+    users_workouts.each do |workout_session|
+      # Note: if the user creates multiple workout sessions at the exact same time, down to the millisecond, then the last one created at that same instant will overwrite all the previous ones.
+
+      workout_date = workout_session.created_at.to_i
+      number_of_sets = workout_session.exercise_instances.length
+
+      if exercise_instances_per_each_workout_session["#{workout_date}"] != nil
+        exercise_instances_per_each_workout_session["#{workout_date}"] += number_of_sets
+      else
+        exercise_instances_per_each_workout_session["#{workout_date}"] = number_of_sets
+      end
+
+    end
+
+    render :json => exercise_instances_per_each_workout_session
   end
 
 
   def create
-    workout_session = WorkoutSession.create({
-      user_id: current_user.id,
-      title: ,
-      workout_id: params[:workout_id] 
-      })
+    # workout_session = WorkoutSession.create({
+    #   user_id: current_user.id,
+    #   title: ,
+    #   workout_id: params[:workout_id] 
+    #   })
     
   end
 end
