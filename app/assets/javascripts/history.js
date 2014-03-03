507e5bd0ed47d810b0068b4a902b2ec7692e1f5d
Jaradus <ethanschmertzler@mac.com>
2014-03-03 13:08:03 -0500

    Exercise ID now included in HTML element

--- app/assets/javascripts/history.js
+++ app/assets/javascripts/history.js
@@ -61,7 +61,7 @@ var workoutHistory = {
       method: 'post',
       data: date_hash,
       success: function(data){
-        // console.log(data)
+        console.log(data)
         self.renderDayData(data)
       }
     })
@@ -76,12 +76,13 @@ var workoutHistory = {
 
     $('.exercise_name').find('a').click(function(e){
       e.preventDefault();
-      console.log($(this)[0].attributes[1].value)
+      var exercise_id = $(this)[0].attributes[1].value;
+      var exercise_modal_target = $(this)[0].attributes[2].value;
     })
-    
+
   },
 
-  visualizeDataForWorkoutOnGivenDate: function(data) {
+  visualizeDataForWorkoutOnGivenDate: function(exercise) {
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
@@ -106,7 +107,7 @@ var workoutHistory = {
         .x(function(d) { return x(d.date); })
         .y(function(d) { return y(d.close); });
 
-    var svg = d3.select("#d3_visualization").append("svg")
+    var svg = d3.select("#progress_line_graph").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -118,8 +119,16 @@ var workoutHistory = {
     //     d.close = +d.close;
     //   });
 
+      d3.json("exercise/"+exercise+"/history", function(error, data) {
+        if (error) return console.warn(error);
+        data.forEach(function(d){
+          d.date = parseDate(d.date);
+          d.weight = +d.weight
+        });
+      });
+
       x.domain(d3.extent(data, function(d) { return d.date; }));
-      y.domain(d3.extent(data, function(d) { return d.close; }));
+      y.domain(d3.extent(data, function(d) { return d.weight; }));
 
       svg.append("g")
           .attr("class", "x axis")
