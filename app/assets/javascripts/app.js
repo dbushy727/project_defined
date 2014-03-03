2f75c311effb4714b61e1d8090243a5451db459a
dbushy727 <d.bushkanets@gmail.com>
2014-03-03 10:39:29 -0500

    select workout goes to corresponding exercises view on mobile

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-
+// 
 var Exercise = Backbone.Model.extend({
   url: '/exercise'
 })
--- app/assets/javascripts/mobile_app.js
+++ app/assets/javascripts/mobile_app.js
@@ -18,6 +18,7 @@ var Splash = Backbone.View.extend({
 var SelectWorkout = Backbone.View.extend({
   initialize: function(){
     this.render();
+    this.chooseExercise();
   },
   render: function(){
     var self = this;
@@ -35,14 +36,20 @@ var SelectWorkout = Backbone.View.extend({
   },
   chooseExercise: function(){
     $('.select_workout').find('.select_rectangle').on("click", function(){
-      workout_id = $(this).find('h3').first().text()
-      workout_title = $(this).find('.workout_id').val()
+      console.log("clicked")
+      var workout_title = $(this).find('h3').first().text()
+      var workout_id = $(this).find('.workout_id').val()
+      new SelectExercise({workout_id: workout_id});
+      $('.select_workout').hide();
+      $('.select_exercise').show();
     })
   }
 
 })
 var SelectExercise = Backbone.View.extend({
-  initialize: function(){
+  initialize: function(params){
+    var self = this;
+    this.workout_id = params.workout_id
     this.render();
   },
   render: function(){
@@ -52,10 +59,18 @@ var SelectExercise = Backbone.View.extend({
       method:   'GET',
       dataType: 'json',
       success: function(data){
-        var source       = $('#exercise_template_mobile').html();
-        var template     = Handlebars.compile(source);
-        var templateData = template(data);
-        $('#exercise_content_mobile').append(templateData);
+        console.log("This is original data",data)
+        console.log("This is the clicked on workout_id")
+        _.each(data.workouts, function(workout){
+          if (workout.id == self.workout_id) {
+            data = {exercises: workout.exercises}
+            var source       = $('#exercise_template_mobile').html();
+            var template     = Handlebars.compile(source);
+            var templateData = template(data);
+            $('#exercise_content_mobile').append(templateData);
+          }
+        })
+        
       }
     }) 
   }
@@ -64,7 +79,7 @@ var SelectExercise = Backbone.View.extend({
 
 $(function(){
   window.select_workout = new SelectWorkout();
-  window.select_exercise = new SelectExercise();
+  // window.select_exercise = new SelectExercise();
   window.splash_page = new Splash();
 
 })
