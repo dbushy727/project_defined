cc38bf3ca521f9abce8ed7e5113d7c63f0fc2ccb
dbushy727 <d.bushkanets@gmail.com>
2014-03-03 11:11:20 -0500

    fixed typo on applicationcontroller

--- app/controllers/workout_lists_controller.rb
+++ app/controllers/workout_lists_controller.rb
@@ -1,11 +1,12 @@
-class WorkoutListsController < ApplicationsController
+class WorkoutListsController < ApplicationController
 
-  def index
-  end
-  def new
-  end
-  def create
-    @workout_list = WorkoutList.create(exercise_id: params[:exercise_id], workout_id: params[:workout_id])
-  end
+  # def index
+
+  # end
+
+
+  # def create
+  #   @workout_list = WorkoutList.create(exercise_id: params[:exercise_id], workout_id: params[:workout_id])
+  # end
   
 end
