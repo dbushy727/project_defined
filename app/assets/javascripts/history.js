9a77fcaf9c6889e0197f54571976233715316fa0
Jaradus <ethanschmertzler@mac.com>
2014-03-03 19:29:10 -0500

    Modal appears on workout click

--- app/assets/javascripts/history.js
+++ app/assets/javascripts/history.js
@@ -4,6 +4,30 @@ var workoutHistory = {
     this.historyHeatmap()
   },
 
+  modalWatcher: function(){
+    $('.reveal-modal').data('reveal-init', {
+        animation: 'fadeAndPop',
+        animation_speed: 250,
+        close_on_background_click: false,
+        close_on_esc: false,
+        dismiss_modal_class: 'close-reveal-modal',
+        bg_class: 'reveal-modal-bg',
+        bg : $('.reveal-modal-bg'),
+        css : {
+            open : {
+                'opacity': 0,
+                'visibility': 'visible',
+                'display' : 'block'
+            },
+            close : {
+                'opacity': 1,
+                'visibility': 'hidden',
+                'display': 'none'
+            }
+        }
+    });
+  },
+
   historyHeatmap: function(){
     var self = this;
 
@@ -74,12 +98,28 @@ var workoutHistory = {
     var templateData = template(data);
     $('#data_table').empty();
     $('#data_table').append(templateData);
+    self.modalWatcher()
 
     $('.exercise_name').find('a').click(function(e){
       e.preventDefault();
       var exercise_id = $(this)[0].attributes[1].value;
       var exercise_modal_target = $(this)[0].attributes[2].value;
-      self.visualizeDataForWorkoutOnGivenDate(exercise_id);
+      $('#'+exercise_modal_target).foundation('reveal', 'open');
+
+      $('a.close-reveal-modal').click(function(e){
+        $('#'+exercise_modal_target).foundation('reveal', 'close');
+        $('a.close-reveal-modal').unbind('click');
+      });
+
+      // Puts an event listener on the window to close the modal
+      $(window).click(function(e){
+        if (e.target.attributes.class.value == "reveal-modal-bg"){
+          $('#'+exercise_modal_target).foundation('reveal', 'close');
+          // Removes the event listener on window close
+          $(window).unbind('click');
+        }
+      })
+      // self.visualizeDataForWorkoutOnGivenDate(exercise_id);
     })
 
   },
@@ -114,17 +154,18 @@ var workoutHistory = {
       var svg = d3.select("#progress_line_graph").append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+          .append("g")
           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       d3.json("exercise/"+exercise+"/history", function(error, json) {
         var data = json.data.exercise_history;
         if (error) return console.warn(error);
         data.forEach(function(d){
-          console.log(d)
+         
           var date     = new Date(d.created_at);
           d.created_at = parseDate(date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear());
-          d.total_reps       = +d.total_reps;
+          d.total_reps = +d.total_reps;
+          
         });
 
         x.domain(d3.extent(data, function(d) { return d.created_at; }));
@@ -147,83 +188,19 @@ var workoutHistory = {
 
         data.forEach(function(d){
           var data_set = {
-                      created_at: d.created_at,
-                      total_reps: d.total_reps
-                      }
+                          created_at: d.created_at,
+                          total_reps: d.total_reps
+                          }
           path_data.push(data_set)
         })
 
+        console.log(path_data)
+
         svg.append("path")
-            .datum(path_data)
+            .datum(data)
             .attr("class", "line")
             .attr("d", line);
       });
-
-// =========================================================================
-
-      // var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      //     width = 960 - margin.left - margin.right,
-      //     height = 500 - margin.top - margin.bottom;
-
-      // var parseDate = d3.time.format("%d-%b-%y").parse;
-
-      // var x = d3.time.scale()
-      //     .range([0, width]);
-
-      // var y = d3.scale.linear()
-      //     .range([height, 0]);
-
-      // var xAxis = d3.svg.axis()
-      //     .scale(x)
-      //     .orient("bottom");
-
-      // var yAxis = d3.svg.axis()
-      //     .scale(y)
-      //     .orient("left");
-
-      // var line = d3.svg.line()
-      //     .x(function(d) { return x(d.created_at); })
-      //     .y(function(d) { return y(d.reps); });
-
-      // var svg = d3.select("#progress_line_graph").append("svg")
-      //     .attr("width", width + margin.left + margin.right)
-      //     .attr("height", height + margin.top + margin.bottom)
-      //   .append("g")
-      //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      // d3.tsv("data.tsv", function(error, data) {
-      //   data.forEach(function(d) {
-      //     d.created_at = parseDate(d.created_at);
-      //     d.reps = +d.reps;
-      //     console.log("Created at: ",d.created_at)
-      //     console.log("Reps: ",d.reps)
-      //   });
-
-      //   x.domain(d3.extent(data, function(d) { return d.created_at; }));
-      //   y.domain(d3.extent(data, function(d) { return d.reps; }));
-
-      //   svg.append("g")
-      //       .attr("class", "x axis")
-      //       .attr("transform", "translate(0," + height + ")")
-      //       .call(xAxis);
-
-      //   svg.append("g")
-      //       .attr("class", "y axis")
-      //       .call(yAxis)
-      //     .append("text")
-      //       .attr("transform", "rotate(-90)")
-      //       .attr("y", 6)
-      //       .attr("dy", ".71em")
-      //       .style("text-anchor", "end")
-      //       .text("Price ($)");
-
-      //   console.log(data)
-
-      //   svg.append("path")
-      //       .datum(data)
-      //       .attr("class", "line")
-      //       .attr("d", line);
-      // });
 
 
     }
